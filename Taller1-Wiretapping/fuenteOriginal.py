d9b6c96a58c000d524d33e9d76aa22e207634161
Diego Sueiro <diegos@semexpert.com.ar>
2020-05-10 11:08:45 -0300

    Agrego contador de frames

--- Taller1-Wiretapping/fuenteOriginal.py
+++ Taller1-Wiretapping/fuenteOriginal.py
@@ -3,13 +3,17 @@
 from math import *
 from scapy.all import *
 
+frames = 0
 S1 = {}
 
 def mostrar_fuente(S):
+	global frames
+	frames += 1
 	N = sum(S.values())
 	simbolos = sorted(S.iteritems(), key=lambda x: -x[1])
 	print "\n".join([ "%s : %.5f" % (d,k/N) for d,k in simbolos ])
-	print
+	print "\n%d" % (frames)
+	print 
 
 def callback(pkt):
 	if pkt.haslayer(Ether):
