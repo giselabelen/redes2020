e3d36be521dbbc18a45d0ae33fd2ba1d4ed0a0cd
giselabelen <gise_5291@yahoo.com.ar>
2020-06-08 17:07:21 -0300

    fix codigos ICMP hexadecimal

--- Taller3-Port Scanning/port_scanner_tcp_udp.py
+++ Taller3-Port Scanning/port_scanner_tcp_udp.py
@@ -8,21 +8,21 @@ ports = range(1025)
 ip = socket.gethostbyname(sys.argv[1])
 #print ip
 
-print "Pruebas TCP"
-for i in ports:
-	p = IP(dst=ip)/TCP(dport=i, flags='S')
-	print i,
+# print "Pruebas TCP"
+# for i in ports:
+# 	p = IP(dst=ip)/TCP(dport=i, flags='S')
+# 	print i,
 
-	resp = sr1(p, verbose=False, timeout=0.2)
-	if resp is None:
-		print "filtrado"
-	elif resp.haslayer(TCP):
-		tcp_layer = resp.getlayer(TCP)
-		if tcp_layer.flags == 0x12:
-			print "abierto", tcp_layer.flags
-			sr1(IP(dst=ip)/TCP(dport=ports, flags='AR'), verbose=False, timeout=1)
-		elif tcp_layer.flags == 0x14:
-			print "cerrado", tcp_layer.flags
+# 	resp = sr1(p, verbose=False, timeout=0.2)
+# 	if resp is None:
+# 		print "filtrado"
+# 	elif resp.haslayer(TCP):
+# 		tcp_layer = resp.getlayer(TCP)
+# 		if tcp_layer.flags == 0x12:
+# 			print "abierto", tcp_layer.flags
+# 			sr1(IP(dst=ip)/TCP(dport=ports, flags='AR'), verbose=False, timeout=1)
+# 		elif tcp_layer.flags == 0x14:
+# 			print "cerrado", tcp_layer.flags
 
 
 
@@ -40,5 +40,7 @@ for i in ports:
 		icmp_layer = resp.getlayer(ICMP)
 		if icmp_layer.type == 0x03 and icmp_layer.code == 0x03:
 			print "cerrado", icmp_layer.flags
-		elif icmp_layer.type == 0x03 and icmp_layer.code in [0x09,0x10,0x13]:
+		elif icmp_layer.type == 0x03 and icmp_layer.code in [0x09,0x0A,0x0D]:
 			print "filtrado"
+		else:
+			print "ICMP ",icmp_layer.code # esto es por si me viene un codigo distinto
